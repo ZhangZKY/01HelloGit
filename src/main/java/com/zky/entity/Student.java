48d76c4c9699309be7b747b4669cdda37d61cfb3
zhangzky <zhangzky@outlook.com>
2021-08-25 14:58:53 +0800

    1.0.1

--- src/main/java/com/zky/entity/Student.java
+++ src/main/java/com/zky/entity/Student.java
@@ -1,5 +1,7 @@
 package com.zky.entity;
 
+import java.util.Objects;
+
 public class Student {
     private Integer id;
     private String name;
@@ -34,5 +36,19 @@ public class Student {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
     }
 }
